54cc8552a8c8c2fad4df596ab10105080e0a2d92
maciekbe1 <madecki1991@gmail.com>
2025-09-13 19:00:21 +0200

    Implement code changes to enhance functionality and improve performance

--- app.config.ts
+++ app.config.ts
@@ -35,6 +35,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     'expo-secure-store',
     'expo-router',
     'expo-web-browser',
+    'expo-font',
   ],
   experiments: {
     typedRoutes: true,
